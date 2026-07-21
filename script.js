d65a858b8b31af71c39e2ad2bfe5033fd3e81c83
Samir Patil <samirpatil1626@gmail.com>
2026-07-21 16:49:18 +0530

    A

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("store-btn").addEventListener("click", () => {
+    window.open("https://shop.garena.sg/?utm_source=official+website&utm_medium=Entrance", "_blank", "noopener,noreferrer");
+});
